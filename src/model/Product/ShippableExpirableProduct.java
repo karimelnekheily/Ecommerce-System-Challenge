aeedbbfeeb6367348499276082c3e2193ae3084b
karim elnekheily <karimmichael2002@yahoo.com>
2025-07-04 21:28:06 +0300

    ShippableExpirableProduct Done

--- src/model/Product/ShippableExpirableProduct.java
+++ src/model/Product/ShippableExpirableProduct.java
@@ -1,16 +1,30 @@
 package model.Product;
 
+import java.time.LocalDate;
+
 public class ShippableExpirableProduct extends product implements ExpireItems,ShippableItems{
 
 
+    private double weight;
+    private LocalDate expiryDate;
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public ShippableExpirableProduct(String name, int price, int quantity, double weight, LocalDate expiryDate) {
+        super(name, price, quantity);
+        this.expiryDate = expiryDate;
+        this.weight = weight;
+    }
 
     @Override
     public boolean isExpired() {
-        return false;
+        return expiryDate.isBefore(LocalDate.now());
     }
 
     @Override
     public double getWeight() {
-        return 0;
+        return weight;
     }
 }
